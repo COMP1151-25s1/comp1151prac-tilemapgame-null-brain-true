d8501407caadbe8def38d013af804e48c16c930a
null-brain-true <minkhant.tun@students.mq.edu.au>
2025-05-08 10:54:48 +1000

    added tilemap, working on player

---
+++ Assets/Tiled/TilemapGame/tilemap.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilemap" tilewidth="16" tileheight="16" spacing="1" tilecount="198" columns="18">
+ <image source="tilemap.png" width="305" height="186"/>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="13" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="2" width="14" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="94">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="2" width="10" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="112">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+</tileset>
